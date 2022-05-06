32957235d6eb145c571cfb7eaf201a0ce21f1fd4
stanciudragosioan <stanciudragosioan92@gmail.com>
2022-05-06 15:13:30 +0300

    added loader

--- mealsToGo/src/features/restaurants/screens/restaurants.screen.js
+++ mealsToGo/src/features/restaurants/screens/restaurants.screen.js
@@ -6,6 +6,7 @@ import { Searchbar } from "react-native-paper";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { SafeArea } from "../../../components/utility/safe-area.component";
+import { ActivityIndicator, Colors } from "react-native-paper";
 const SearchContainer = styled(Text)`
   padding: 16px;
 `;
@@ -19,6 +20,15 @@ const RestaurantList = styled(FlatList).attrs({
     padding: 16,
   },
 })``;
+
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
 export const RestaurantsScreen = () => {
   const [searchQuery, setSearchQuery] = React.useState("");
 
@@ -28,6 +38,11 @@ export const RestaurantsScreen = () => {
 
   return (
     <SafeArea>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading size={50} animating={true} color={Colors.blue300} />
+        </LoadingContainer>
+      )}
       <SearchContainer>
         <Search
           placeholder="Search"
