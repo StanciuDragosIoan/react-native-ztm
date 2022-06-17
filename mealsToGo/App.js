3ff61054f8fab0a6a76c3a313b8fd8133ff99a11
stanciudragosioan <stanciudragosioan92@gmail.com>
2022-06-17 13:41:45 +0300

    first test authentication firebase

--- mealsToGo/App.js
+++ mealsToGo/App.js
@@ -1,7 +1,12 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
+// import firebase from "firebase/compat/app";
+// import "firebase/compat/auth";
+// import "firebase/compat/firestore";
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import {
   useFonts as useOswald,
   Oswald_400Regular,
@@ -11,7 +16,40 @@ import { RestaurantsContextProvider } from "./src/services/restaurants/restauran
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
 import { Navigation } from "./src/infrastructure/navigation";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDC2X-AAN5Nnu0XpHiLLWHl0MXIfNcSI3k",
+  authDomain: "mealstogo-78cce.firebaseapp.com",
+  projectId: "mealstogo-78cce",
+  storageBucket: "mealstogo-78cce.appspot.com",
+  messagingSenderId: "389868775204",
+  appId: "1:389868775204:web:4841564bcf91346015b5b1",
+};
+// if (!firebase.getApps.length) {
+//   initializeApp(firebaseConfig);
+// }
+
+initializeApp(firebaseConfig);
+
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(() => {
+    console.log("MOUTN");
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, "test@test.com", "test123")
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log("LOGGED IN");
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(error);
+        console.log("NOT LOGGED IN");
+      });
+  }, []);
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
@@ -23,6 +61,8 @@ export default function App() {
   if (!oswaldLoaded || !latoLoaded) {
     return null;
   }
+
+  // if (!isAuthenticated) return null;
 
   return (
     <>
